89efec847a5b432db00e843933ee8c6aa968127f
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-05 20:53:20 +0000

    updated to include uuid and appropriate typings.

--- src/renderer.ts
+++ src/renderer.ts
@@ -32,6 +32,8 @@ import "bootstrap/dist/css/bootstrap";
 import './index.css';
 import Text, { TextType } from './components/text/Text';
 import Image, { ImageProps } from './components/image/Image';
+// import Project from './lib/project/Project';
+import Base from './lib/base/Base';
 
 const app : HTMLDivElement | null = document.getElementById('app') as HTMLDivElement;
 
@@ -87,18 +89,44 @@ if(app) {
             app.append(createImage(e));
         }
     })
-    // const TextHandler = {
-    //     get: function(target: any, prop: any, receiver: any) {
-    //         console.log(prop );
-    //     }
-    // }
-    // const p = new Proxy(t, TextHandler);
-    // console.log('proxy', p);
 
-    // console.log(t);
+    interface TestProps {
+        text?: string;
+        onClick?: (e: EventTarget) => void
+    }
 
-    // t.text = "Hello World";
+    class Test extends Base<TestProps> {
+        constructor(props: TestProps) {
+            super(props);
+            this.defaultProps = {
+                text: "Testing Default",
+                onClick : () => {
+                    console.log("Hello World!")
+                }
+            }
+            this.state = {
+                test: true
+            }
+        }
 
+        test() {
+            
+            const [value, updateValue] = this.setState<boolean>(false);            console.log(value);
+            updateValue(true);
+            this.state.test = "asda";
+        }
+    }
+
+    const t2 = new Test({text: "hello", onClick : () => { console.log('asdasd')}});
+    t2.test();
+    console.log(t2.props.text);
+    t2.props.text = "Mohammed";
+    console.log(t2.props.text);
+    // const p = new Project({
+    //     name : "Hello World"
+    // });
+
+    // console.log(p);
     
     console.log(t);
     
