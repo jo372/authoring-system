9b743822f8257c1522978182b7e1d630e20330b9
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-11-13 16:31:38 +0000

    updated to remove the state, this might be readded eventually.

--- src/lib/base/Base.ts
+++ src/lib/base/Base.ts
@@ -3,11 +3,9 @@ import { v4 as uuidv4 } from 'uuid';
 abstract class Base<P = {}, S = {}> {
     private _defaultProps: Partial<P> = {};
     private _props : Partial<P> = {};
-    private _state : Partial<S> = {};
 
-    constructor(props: Partial<P>, state?: Partial<S>) {
+    constructor(props: Partial<P>) {
         this._props = props;
-        this._state = state ?? {};
     }
     public get props() : Partial<P> {
         return this._props;
@@ -20,25 +18,6 @@ abstract class Base<P = {}, S = {}> {
     } 
     protected set defaultProps(config: Partial<P>) {
         this._defaultProps = {...config, ...this._props};
-    }
-    protected get state() {
-        return this._state;
-    }
-    protected set state(state: Record<any, any>) {
-        this._state = {...this._state, ...state};
-    }
-    protected setState<T>(value: T) : [T, (newValue: T) => void] {
-        
-        // return [value, updateC]
-
-        // this.state = {...this.state, ...newState};
-        const uuid = uuidv4();
-        console.log(`old uuid`, uuid)
-        const updateCB = (newValue: T) => {
-            this._state[uuid] = newValue;
-            console.log(this._state[uuid]);
-        }
-        return [value, updateCB];
     }
 }
 
