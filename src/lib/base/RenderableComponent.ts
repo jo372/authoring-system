f5dfa47d950e72ca4fd8abe2e19aca3f14d0b156
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-12-14 10:38:50 +0000

    updated to include a private child variable as well as supporting methods for adding, removing and getting the children.

--- src/lib/base/RenderableComponent.ts
+++ src/lib/base/RenderableComponent.ts
@@ -3,13 +3,15 @@ import { v4 as uuidv4 } from 'uuid';
 abstract class RenderableComponent<P = {}> {
     private _defaultProps: Partial<P> = {};
     private _props : Partial<P> = {};
-    
+    private _children: RenderableComponent[] = [];
     constructor(props: Partial<P>) {
         this._props = props;
     }
+    
     public get props() : Partial<P> {
         return this._props;
     }
+
     private set props(props: Partial<P>) {
         this._props = {...props, ...this._defaultProps};
     }
@@ -19,6 +21,23 @@ abstract class RenderableComponent<P = {}> {
     protected set defaultProps(config: Partial<P>) {
         this._defaultProps = {...config, ...this._props};
     }
+
+    public addChild<C extends RenderableComponent>(child: C) {
+        this._children.push(child);
+    }
+
+    public removeChild<C extends RenderableComponent>(child: C) {
+        this._children = this._children.filter(c => c !== child);
+    }
+
+    public removeChildAtIndex<C extends RenderableComponent>(index: number) {
+        this._children.splice(index, 1);
+    }
+
+    public get children() : RenderableComponent[] {
+        return this._children;
+    }
+
 }
 
 export default RenderableComponent;
