3bc5292ca793bdea97ae1d18ec617cb7fe57ed5c
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-12-14 16:03:40 +0000

    updated removeChildAtIndex to remove a redundant extension where it is never referenced. Also updated to merge props into default props.

--- src/lib/base/RenderableComponent.ts
+++ src/lib/base/RenderableComponent.ts
@@ -1,5 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
-
 abstract class RenderableComponent<P = {}> {
     private _defaultProps: Partial<P> = {};
     private _props : Partial<P> = {};
@@ -15,13 +13,16 @@ abstract class RenderableComponent<P = {}> {
     private set props(props: Partial<P>) {
         this._props = {...props, ...this._defaultProps};
     }
+
     protected get defaultProps() : Partial<P> {
         return this._defaultProps;
-    } 
+    }
+    
     protected set defaultProps(config: Partial<P>) {
         this._defaultProps = {...config, ...this._props};
+        this.props = this.defaultProps;
     }
-
+    
     public addChild<C extends RenderableComponent>(child: C) {
         this._children.push(child);
     }
@@ -30,7 +31,7 @@ abstract class RenderableComponent<P = {}> {
         this._children = this._children.filter(c => c !== child);
     }
 
-    public removeChildAtIndex<C extends RenderableComponent>(index: number) {
+    public removeChildAtIndex(index: number) {
         this._children.splice(index, 1);
     }
 
