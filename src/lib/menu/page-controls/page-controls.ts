0c257926a17fbb517d6b0654de57a9e580e8c321
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2022-01-26 23:58:33 +0000

    updated to include some basic functionality when the edit button is presssed. This allows the user to edit the page name.

--- src/lib/menu/page-controls/page-controls.ts
+++ src/lib/menu/page-controls/page-controls.ts
@@ -1,4 +1,5 @@
 import Modal from '../../modal/Modal';
+import './page-controls.css';
 
 interface PageControlsProps {
     text ?: string,
@@ -21,12 +22,41 @@ export const PageControlItem = (props?: PageControlsProps) => {
     template.innerHTML = html;
 
     const pageControl = template.content.firstChild as HTMLLIElement;
+    const currentNavLogo = document.querySelector('.main__content__nav-logo') as HTMLDivElement;
+
+    const updatePageNameTo = (name: string) => {
+        if(currentNavLogo) {
+            const currentPageName = (pageControl.querySelector('span') as HTMLSpanElement).textContent ?? "";
+            currentNavLogo.innerText = currentPageName;
+        }
+    }
+
+    pageControl.addEventListener('click', () => {
+        const currentActiveElement = document.querySelector('.page-controls__list-item.active');
+        if(currentActiveElement) {
+            currentActiveElement.classList.remove('active');
+        }
+        pageControl.classList.add('active');
+        updatePageNameTo(pageControl.querySelector('span')?.textContent ?? "");
+    });
     const listItems = Array.from(pageControl.querySelectorAll('li') as NodeListOf<HTMLLIElement>) ?? [];
 
-    const cloneButton = () => {
-        modal.show();
+    const cloneButton = () => {}
+    const editButton = () => {
+        const currentPageName = (pageControl.querySelector('span') as HTMLSpanElement);
+        if(currentPageName) {
+            currentPageName.setAttribute('contenteditable', '');
+            currentPageName.focus();
+            const keyEventListener = (e: KeyboardEvent) => {
+                if(e.key === 'Enter') {
+                    currentPageName.removeAttribute('contenteditable');
+                    updatePageNameTo(pageControl.querySelector('span')?.textContent ?? "");
+                    currentPageName.removeEventListener('keydown', keyEventListener);
+                }
+            }
+            currentPageName.addEventListener('keydown', keyEventListener);
+        }
     }
-    const editButton = () => {}
     const deleteButton = () => {
         modal.setTitle("Warning: This will delete the page");
         modal.setMessage("Are you sure you want to delete this page?");
